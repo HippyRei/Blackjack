ce175497b9f28ab1ca42f3c351418b53bc41ebeb
reilly_mulligan <reillymull@gmail.com>
2017-01-21 23:48:07 -0800

    Wrote test code for Card, wrote first iteration of Deck, added DeckTest class

--- src/cards/Deck.java
+++ src/cards/Deck.java
@@ -15,18 +15,63 @@ public class Deck {
 	}
 	
 	public Deck(int n) {
+		if (n < 0) {
+			throw new IllegalArgumentException("Must specify a non-negative number of decks");
+		}
+		
 		cards = new LinkedList<Card>();
 		
 		for (int i = 0; i < n; i++) {
-			for (Suit s: Suit.values()) {
-				for (Value v: Value.values()) {
+			for (Suit s : Suit.values()) {
+				for (Value v : Value.values()) {
 					cards.add(new Card(v, s));
 				}
 			}
 		}
 	}
 	
+	public Deck(List<Card> cards) {
+		for (Card c : cards) {
+			if (c == null) {
+				throw new IllegalArgumentException("Deck cannot contain null cards");
+			}
+		}
+		this.cards = cards;
+	}
+	
+	public int numCards() {
+		return cards.size();
+	}
+	
 	public void shuffle() {
 		Collections.shuffle(cards);
+	}
+	
+	public Card draw() {
+		if (cards.size() == 0) {
+			return null;
+		}
+		
+		return cards.remove(0);
+	}
+	
+	public void addTop(Card c) {
+		add(0, c);
+	}
+	
+	public void addBot(Card c) {
+		add(-1, c);
+	}
+	
+	private void add(int pos, Card c) {
+		if (c == null) {
+			throw new IllegalArgumentException("Card cannot be null");
+		}
+		
+		if (pos == -1) {
+			cards.add(c);
+		} else {
+			cards.add(pos, c);
+		}
 	}
 }
--- src/tests/CardTest.java
+++ src/tests/CardTest.java
@@ -1,20 +1,37 @@
 package tests;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import cards.Card;
 import cards.Card.Suit;
 import cards.Card.Value;
 
 public class CardTest {
+	Card c = new Card(Value.ACE, Suit.SPADES);
 
 	@Test(expected = IllegalArgumentException.class)
 	public void constrNullVal() {
-		Card c = new Card(null, Suit.CLUBS);
+		new Card(null, Suit.CLUBS);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void constrNullSuit() {
-		Card c = new Card(Value.ACE, null);
+		new Card(Value.ACE, null);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void constrNullValSuit() {
+		new Card(null, null);
+	}
+	
+	@Test
+	public void val() {
+		assertEquals(c.val(), Value.ACE);
+	}
+	
+	@Test
+	public void suit() {
+		assertEquals(c.suit(), Suit.SPADES);
 	}
 }
